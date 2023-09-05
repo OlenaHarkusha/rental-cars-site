7567c275ff83fbf90c0eae5389bc3b0b06326a51
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-09-05 23:06:56 +0300

    Create globalStyles.js

---
+++ src/globalStyles.js
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from "styled-components";
+import "modern-normalize";
+
+export const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  padding: 20px;
+  font-family: 'Manrope', sans-serif;
+
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  
+}
+
+code {
+font-family: 'Manrope', sans-serif;
+}
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin: 0;
+}
+ul,
+ol {
+  margin: 0;
+  padding-left: 0;
+  list-style-type: none;
+}
+button {
+  cursor: pointer;
+}
+address {
+  font-style: normal;
+}
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+a {
+  text-decoration: none;
+}
+`;
