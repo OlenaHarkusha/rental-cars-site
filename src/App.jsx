cc804aee7b0528dc62894639dc30178cfef561d4
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-09-05 23:06:45 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./components/pages/HomePage";
+import { CatalogPage } from "./components/pages/CatalogPage";
+import { FavoritesPage } from "./components/pages/FavoritesPage";
+import { Layout } from "./components/Layout";
+import { GlobalStyle } from "./globalStyles";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./components/Theme";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/catalog" element={CatalogPage} />
+          <Route path="/favorites" element={FavoritesPage} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
+  );
+};
