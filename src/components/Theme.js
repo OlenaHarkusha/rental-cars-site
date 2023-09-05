0fbc6fa9bb190f7b343f96875b13830a564c1d22
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-09-05 23:06:50 +0300

    Create Theme.js

---
+++ src/components/Theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    mainFont: "#121417",
+    accentFont: "#ffffff",
+    accent: "#3470FF",
+    accentHover: "#0B44CD",
+  },
+  radii: {
+    normal: "12px",
+    large: "24px",
+  },
+  fontSizes: { xs: "12px", s: "14px", m: "16px", l: "18px" },
+  fontWeights: {
+    normal: 400,
+    medium: 500,
+    semiBold: 600,
+  },
+};
