c7e5c461752ded44bd797986d19fdf83aa3b8257
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-09-06 23:55:33 +0300

    Update Theme.js

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -4,15 +4,25 @@ export const theme = {
     accentFont: "#ffffff",
     accent: "#3470FF",
     accentHover: "#0B44CD",
+    inputBackground: "#F7F7FB",
+    backdropBackground: "#12141780",
   },
   radii: {
     normal: "12px",
     large: "24px",
   },
-  fontSizes: { xs: "12px", s: "14px", m: "16px", l: "18px" },
+  fontSizes: { xs: "12px", s: "14px", m: "16px", l: "18px", xl: "24px" },
   fontWeights: {
     normal: 400,
     medium: 500,
     semiBold: 600,
   },
+  // devices: {
+  //   xs: `(min-width: 320px)`,
+  //   sm: `(min-width: 640px)`,
+  //   md: `(min-width: 768px)`,
+  //   lg: `(min-width: 1024px)`,
+  //   xl: `(min-width: 1280px)`,
+  //   "2xl": `(min-width: 1536px)`,
+  // },
 };
