4ee3d1309e78067a1c0d87659b6db109b5513197
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-09-07 23:27:39 +0300

    Update operations.js

--- src/redux/catalogSlice/operations.js
+++ src/redux/catalogSlice/operations.js
@@ -1,11 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://6478e3f3362560649a2e9a16.mockapi.io/adverts";
+axios.defaults.baseURL = "https://6478e3f3362560649a2e9a16.mockapi.io/";
 
 export const fetchAdverts = createAsyncThunk("catalog/fetchAll", async (_, thunkAPI) => {
   try {
-    const response = await axios.get("/");
+    const response = await axios.get("adverts");
     return response.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
