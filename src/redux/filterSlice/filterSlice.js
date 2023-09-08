e6dbb7c3c877668986968ddc4bcd35f2478c5fce
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-09-09 02:00:17 +0300

    Update filterSlice.js

--- src/redux/filterSlice/filterSlice.js
+++ src/redux/filterSlice/filterSlice.js
@@ -12,17 +12,8 @@ export const filterSlice = createSlice({
     filterAdverts(state, action) {
       state.brand = action.payload.brand;
       state.price = action.payload.price;
-      state.from = action.payload.from;
-      state.to = action.payload.to;
-      // },
-      // filterPrice(state, action) {
-      //   state.price = action.payload;
-      // },
-      // filterMilleageFrom(state, action) {
-      //   state.from = action.payload;
-      // },
-      // filterMilleageTo(state, action) {
-      //   state.to = action.payload;
+      state.from = Number(action.payload.from);
+      state.to = Number(action.payload.to);
     },
   },
 });
