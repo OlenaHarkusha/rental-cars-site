cf8ea73c4b90bc3b2f5ed7873f2224709a8a7131
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-09-09 01:59:43 +0300

    Update FavoritesPage.jsx

--- src/components/pages/FavoritesPage/FavoritesPage.jsx
+++ src/components/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,5 +1,10 @@
 // import React from "react";
 
+import { useSelector } from "react-redux";
+import { selectFavorites } from "../../../redux/selectors";
+import { Catalog } from "../../Catalog/Catalog";
+
 export const FavoritesPage = () => {
-  return <div>FavoritesPage</div>;
+  const favoriteAdverts = useSelector(selectFavorites);
+  return <Catalog adverts={favoriteAdverts} />;
 };
