b87f4bdce9f11e9e050440d131bc1d8142b0cdc2
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-09-10 00:02:32 +0300

    Update operations.js

--- src/redux/catalogSlice/operations.js
+++ src/redux/catalogSlice/operations.js
@@ -5,9 +5,18 @@ axios.defaults.baseURL = "https://6478e3f3362560649a2e9a16.mockapi.io/";
 
 export const fetchAdverts = createAsyncThunk("catalog/fetchAll", async (page, thunkAPI) => {
   try {
-    const response = await axios.get(`/adverts?page=${page}&limit=8`);
-    return { data: response.data, page };
+    const response = await axios.get(`/adverts`);
+    return response.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
   }
 });
+
+// export const fetchAdverts = createAsyncThunk("catalog/fetchAll", async (page, thunkAPI) => {
+//   try {
+//     const response = await axios.get(`/adverts?page=${page}&limit=8`);
+//     return { data: response.data, page };
+//   } catch (error) {
+//     return thunkAPI.rejectWithValue(error.message);
+//   }
+// });
