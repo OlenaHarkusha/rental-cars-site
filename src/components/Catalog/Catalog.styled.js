8ddb43b222a653b2c87f63905aecd9a6cabd56c9
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-09-10 00:01:46 +0300

    Update Catalog.styled.js

---
+++ src/components/Catalog/Catalog.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const CatalogContainer = styled.div`
+  width: 100%;
+  padding: 0 10px;
+  margin-top: 50px;
+`;
+
+export const CatalogList = styled.ul`
+  /* display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 29px;
+  grid-row-gap: 50px; */
+`;
