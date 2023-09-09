eee514e522cf7b7bce93e8165201bff4586562d3
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-09-10 02:47:56 +0300

    Update Catalog.styled.js

--- src/components/Catalog/Catalog.styled.js
+++ src/components/Catalog/Catalog.styled.js
@@ -1,15 +1,24 @@
 import styled from "styled-components";
+import { devices } from "../constants/devices";
 
 export const CatalogContainer = styled.div`
   width: 100%;
   padding: 0 10px;
-  margin-top: 50px;
+  margin-top: 100px;
 `;
 
 export const CatalogList = styled.ul`
-  /* display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: repeat(2, 1fr);
-  grid-column-gap: 29px;
-  grid-row-gap: 50px; */
+  @media ${devices.md} {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+    grid-column-gap: 29px;
+    grid-row-gap: 50px;
+
+    margin-bottom: 50px;
+  }
+  @media ${devices.xl} {
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+  }
 `;
