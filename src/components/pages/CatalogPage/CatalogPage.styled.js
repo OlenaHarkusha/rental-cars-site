62dcef88192a4284928d0c8ffa1c07f86bd80a04
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-09-10 00:02:16 +0300

    Update CatalogPage.styled.js

--- src/components/pages/CatalogPage/CatalogPage.styled.js
+++ src/components/pages/CatalogPage/CatalogPage.styled.js
@@ -1,5 +1,22 @@
 import styled from "styled-components";
+import { devices } from "../../constants/devices";
 
 export const Container = styled.div`
-  padding-top: 70px;
+  min-height: 100vh;
+  padding: 75px 0;
+  width: 100%;
+  margin: 0 auto;
+
+  @media ${devices.xs} {
+    width: 320px;
+  }
+
+  @media ${devices.md} {
+    width: 768px;
+  }
+
+  @media ${devices.xl} {
+    padding: 100px 0 0 0;
+    width: 1280px;
+  }
 `;
