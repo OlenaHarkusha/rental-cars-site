918c8188470cfb109c4394fc5c0febeca0b23fe8
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-09-10 02:48:17 +0300

    Update Theme.js

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -24,12 +24,4 @@ export const theme = {
     medium: 500,
     semiBold: 600,
   },
-  // devices: {
-  //   xs: `(min-width: 320px)`,
-  //   sm: `(min-width: 640px)`,
-  //   md: `(min-width: 768px)`,
-  //   lg: `(min-width: 1024px)`,
-  //   xl: `(min-width: 1280px)`,
-  //   "2xl": `(min-width: 1536px)`,
-  // },
 };
